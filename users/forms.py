f1011d311eb8f12cb5ae61864fea886ac47bcca0
bshisia <shisiabrian7@gmail.com>
2025-07-29 01:55:01 +0300

    fix: fix the user registration

--- users/forms.py
+++ users/forms.py
@@ -55,7 +55,7 @@ class CompanySignUpForm(UserCreationForm):
         ('Electricity', 'Electricity'),
         ('Gardening', 'Gardening'),
         ('Home Machines', 'Home Machines'),
-        ('Housekeeping', 'Housekeeping'),
+        ('House Keeping', 'House Keeping'),
         ('Interior Design', 'Interior Design'),
         ('Locks', 'Locks'),
         ('Painting', 'Painting'),
