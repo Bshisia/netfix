e40207b8e1cec202805f667063443814f8899340
bshisia <shisiabrian7@gmail.com>
2025-07-28 21:29:04 +0300

    Add edit service URL pattern to urlpatterns

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', v.service_list, name='services_list'),
     path('create/', v.create, name='services_create'),
     path('<int:id>', v.index, name='index'),
+    path('<int:id>/edit/', v.edit_service, name='edit_service'),
     path('<int:id>/request_service/', v.request_service, name='request_service'),
     path('<slug:field>/', v.service_field, name='services_field'),
 ]
