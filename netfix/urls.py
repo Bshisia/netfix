028d3b3df5a8774def82c98741ecb02c6b033e5f
bshisia <shisiabrian7@gmail.com>
2025-07-28 21:43:29 +0300

    Add static file serving in development mode to urls.py

--- netfix/urls.py
+++ netfix/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views as v
 
@@ -27,3 +29,6 @@ urlpatterns = [
     path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
     path('company/<slug:name>', v.company_profile, name='company_profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
